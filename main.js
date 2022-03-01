904ddb036ef2d76efc727d5207fad06b46bf4f5f
Andrii Oriekhov <andriyorehov@gmail.com>
2022-03-01 11:27:11 +0200

    add run func

--- main.js
+++ main.js
@@ -14,16 +14,9 @@ const getRandomInt = (min, max) => {
 
 const RESOURCE_EXCLUSTIONS = ['image', 'media', 'font', 'other'];
 
-(async () => {
-  const words = fs.readFileSync('stop_words_russian.txt', 'utf-8').split('\n');
-  const browser = await chromium.launch(
-    {
-      headless: false
-    }
-  );
 
-  while (true) {
-    const page = await browser.newPage();
+const run = async (browser, words) => {
+  const page = await browser.newPage();
 
     await page.route('**/*', (route) => {
       return RESOURCE_EXCLUSTIONS.includes(route.request().resourceType())
@@ -50,6 +43,17 @@ const RESOURCE_EXCLUSTIONS = ['image', 'media', 'font', 'other'];
         await page.close();
       }
     });
+}
 
+(async () => {
+  const words = fs.readFileSync('stop_words_russian.txt', 'utf-8').split('\n');
+  const browser = await chromium.launch(
+    {
+      headless: false
+    }
+  );
+
+  while (true) {
+    await Promise.all([run(browser, words)]);
   }
 })();
